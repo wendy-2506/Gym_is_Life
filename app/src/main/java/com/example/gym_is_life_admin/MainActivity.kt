0ad0d5d68b58e521995509f1a82ccb1cc7146489
Wendy Mendoza Pérez <wendy_25_06@outlook.com>
2022-06-12 21:42:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gym_is_life_admin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gym_is_life_admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
