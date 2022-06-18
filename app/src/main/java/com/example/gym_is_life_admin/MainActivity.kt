6df5adc38659bab90ddaae6b49b76ce3d5323c49
GUILLERMO <diazbeltrang@gmail.com>
2022-06-18 11:12:49 -0500

    actualizado 18/06 11:12am

--- app/src/main/java/com/example/gym_is_life_admin/MainActivity.kt
+++ app/src/main/java/com/example/gym_is_life_admin/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.gym_is_life_admin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.gym_is_life_admin.Login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnIniciarSesión: Button = findViewById(R.id.btnIniciarSesion)
+        btnIniciarSesión.setOnClickListener{
+            this.irActividad();
+        }
+    }
+    private fun irActividad(){
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
     }
 }
