0241a5a6be6cc687958fc4d2edc44972aa71daf4
GUILLERMO <diazbeltrang@gmail.com>
2022-06-18 10:55:55 -0500

    actualizado 18/06 10:55am

---
+++ app/src/main/java/com/example/gym_is_life_admin/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gym_is_life_admin.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gym_is_life_admin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
