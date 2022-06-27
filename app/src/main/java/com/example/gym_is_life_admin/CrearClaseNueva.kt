8b3bc4a5a2fb61f581afb8707c7e12b8b6354136
Wendy Mendoza <20100957@ue.edu.pe>
2022-06-27 00:50:49 -0500

    actualizacion de fragment usuarios y actividades 27/6 12.46am

---
+++ app/src/main/java/com/example/gym_is_life_admin/CrearClaseNueva.kt
@@ -0,0 +1,35 @@
+package com.example.gym_is_life_admin
+
+import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class CrearClaseNueva : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_clase_nueva)
+
+        val btnAceptar = findViewById<View>(R.id.btnAceptar) as Button
+
+        btnAceptar.setOnClickListener(){
+            this.AgregarClaseNueva();
+        }
+
+    }
+
+    private fun AgregarClaseNueva(){
+        val builder = AlertDialog.Builder(this)
+        val intent = Intent(this, MainActivity::class.java)
+        builder.setTitle("Clase Nueva creada")
+        builder.setMessage("La clase nueva ha sido creada correctamente.")
+//builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+        builder.setPositiveButton("Ok", null)
+        builder.create()
+        builder.show()
+
+        //aqui deberia abrir de nuevo el fragment
+    }
+}
--- app/src/main/java/com/example/gym_is_life_admin/InicioAdmin/ui/fragments/UsuariosFragment.kt
+++ app/src/main/java/com/example/gym_is_life_admin/InicioAdmin/ui/fragments/UsuariosFragment.kt
@@ -1,10 +1,14 @@
 package com.example.gym_is_life_admin.InicioAdmin.ui.fragments
 
 import android.os.Bundle
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.gym_is_life_admin.AdminRenovarMembresia
+import com.example.gym_is_life_admin.CrearClaseNueva
 import com.example.gym_is_life_admin.R
 
 class UsuariosFragment : Fragment() {
@@ -15,5 +19,13 @@ class UsuariosFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_usuarios, container, false)
-    }
+
+        val btnVer = view?.findViewById<View>(R.id.btnVer) as Button
+
+        //Asigna listener para poder abrir Activity.
+        btnVer.setOnClickListener{ view: View ->
+        val intent = Intent (activity , AdminRenovarMembresia::class.java)
+        activity?.startActivity(intent)
+        }
+}
 }
