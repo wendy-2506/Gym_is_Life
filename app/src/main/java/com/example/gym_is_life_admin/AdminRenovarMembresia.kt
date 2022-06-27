2af57bffbd0b7cdc0ce5b17e1a97b3b2f5c142d8
Wendy Mendoza <20100957@ue.edu.pe>
2022-06-26 22:49:50 -0500

    color de fondo a todo y frame usuarios, iconos 26/6 7.54pm

---
+++ app/src/main/java/com/example/gym_is_life_admin/AdminRenovarMembresia.kt
@@ -0,0 +1,47 @@
+package com.example.gym_is_life_admin
+
+//import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gym_is_life_admin.R.layout.activity_admin_renovar_membresia
+import com.example.gym_is_life_admin.R
+
+class AdminRenovarMembresia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_admin_renovar_membresia)
+
+        val btnVolverAdmin: Button = findViewById(R.id.btnVolverAdmin)
+        val btnActualizarM: Button = findViewById(R.id.btnActualizarM)
+
+        btnVolverAdmin.setOnClickListener(){
+            this.onBackPressed();
+        }
+
+        btnActualizarM.setOnClickListener(){
+            this.ActualizarM();
+        }
+    }
+
+
+    private fun ActualizarM(){
+        val builder = AlertDialog.Builder(this)
+        val intent = Intent(this, MainActivity::class.java)
+        builder.setTitle("Androidly Alert")
+        builder.setMessage("¿Estás seguro que desea actualizar la membresía?")
+//builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+        builder.setPositiveButton("Si") { dialog, which ->
+            startActivity(intent)
+        }
+
+        builder.setNegativeButton("No") { dialog, which ->
+        }
+
+        builder.show()
+    }
+
+}
