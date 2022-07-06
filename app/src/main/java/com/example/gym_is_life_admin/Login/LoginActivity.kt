1c2e4584df5188ae19116a53dc260518ba49aecd
AntonnyFCC <89750595+AntonnyFCC@users.noreply.github.com>
2022-07-06 17:34:23 -0500

    Login discrimination with DB 05/07 17:34

--- app/src/main/java/com/example/gym_is_life_admin/Login/LoginActivity.kt
+++ app/src/main/java/com/example/gym_is_life_admin/Login/LoginActivity.kt
@@ -1,14 +1,20 @@
 package com.example.gym_is_life_admin.Login
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.`AppCompatEditText$InspectionCompanion`
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.gym_is_life_admin.Inicio.InicioActivity
 import com.example.gym_is_life_admin.InicioAdmin.AdminActivity
 import com.example.gym_is_life_admin.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +25,47 @@ class LoginActivity : AppCompatActivity() {
         val plContraseña: EditText = findViewById(R.id.plContraseña)
         val btnIniciar: Button = findViewById(R.id.btnIniciar)
 
+        val db = Firebase.firestore
+
         btnIniciar.setOnClickListener {
-            val intent = Intent(this, AdminActivity::class.java)
-            startActivity(intent)
+            var dni: Int = plUsuario.text.toString().toInt()
+            var cont: String = plContraseña.text.toString()
+
+            db.collection("usuario")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+
+                        if(dni == document.data["dni"].toString().toInt() && cont == document.data["contrase"].toString()){
+                            //Toast.makeText(plUsuario.context,"Incio de sesión exitoso", Toast.LENGTH_LONG).show()
+
+                            if(document.data["tipo_user"].toString().toBoolean()){
+                                val intent = Intent(this, AdminActivity::class.java)
+                                intent.putExtra("dni", document.data["dni"].toString().toInt())
+                                startActivity(intent)
+                                break
+                            }else{
+                                val intent = Intent(this, InicioActivity::class.java)
+                                intent.putExtra("dni", document.data["dni"].toString().toInt())
+                                startActivity(intent)
+                            }
+
+                        }
+                    }
+
+
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
+
+
+
+
+
         }
+
+
 
     }
 }
