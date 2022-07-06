1707b19f770e814dd970100f3c8f01f44b859896
GUILLERMO <diazbeltrang@gmail.com>
2022-07-06 16:47:26 -0500

    Actualizado 06/07 16:48 BD con registroUsuario

---
+++ app/src/main/java/com/example/gym_is_life_admin/Registrarse/models/UsuarioModel.kt
@@ -0,0 +1,11 @@
+package com.example.gym_is_life_admin.Registrarse.models
+
+data class UsuarioModel(
+    val apellido: String,
+    val nombre: String,
+    val correo: String,
+    val dni: Int,
+    val contrase: String,
+    val estado: String,
+    val tipo_user: Boolean
+)
