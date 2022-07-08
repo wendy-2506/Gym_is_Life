248e5332f83db422fb4efaefb530c9543cc359b4
GUILLERMO <diazbeltrang@gmail.com>
2022-07-08 13:13:41 -0500

    Actualizado 07/07 21:18 fragment perfil arreglado

---
+++ app/src/main/java/com/example/gym_is_life_admin/Inicio/ ModelUsuario/Clases.kt
@@ -0,0 +1,10 @@
+package com.example.gym_is_life_admin.Inicio.ModelUsuario
+
+data class Clases(
+    val Disciplina: String,
+    val Instructor: String,
+    val Horario: String,
+    val Salon: String,
+    val Nivel: String,
+    val Aforo: Int,
+)
