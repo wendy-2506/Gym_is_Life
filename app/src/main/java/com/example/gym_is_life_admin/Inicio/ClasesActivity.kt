eead6bec8190bffe45ab50ae87ee03f097e14bb7
GUILLERMO <diazbeltrang@gmail.com>
2022-07-08 11:15:06 -0500

    Actualizado 07/07 21:18 fragment perfil arreglado

---
+++ app/src/main/java/com/example/gym_is_life_admin/Inicio/ClasesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gym_is_life_admin.Inicio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gym_is_life_admin.R
+
+class ClasesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clases)
+    }
+}
